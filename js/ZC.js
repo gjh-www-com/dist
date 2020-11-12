2f92fd0b203f8fc11288981d96fa3d820c045558
guo <guo2153310196@163.com>
2020-11-12 21:59:55 +0800

    2020.11.12

--- js/ZC.js
+++ js/ZC.js
@@ -1,1 +1,1 @@
-"use strict";$(function(){$("#btn").click(function(){$.get("http://jx.xuzhixiang.top/ap/api/reg.php",{username:$("#tet").val(),password:$("#password").val()},function(t){1==t.code?(alert("注册成功跳转到首页"),location.href="SY.html"):alert("注册失败，请重试")})}),$("#password").change(function(){})});
+"use strict";$(function(){$("#btn").click(function(){$.get("http://jx.xuzhixiang.top/ap/api/reg.php",{username:$("#tet").val(),password:$("#password").val()},function(t){1==t.code?(alert("注册成功"),location.href="DL.html"):alert("注册失败，请重试")})})});
